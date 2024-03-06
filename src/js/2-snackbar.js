2d5604119f275a8aebb037fe15692b831e7865d1
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2024-03-06 13:47:07 +0300

    update snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onForSubmit);
+
+function onForSubmit(event) {
+    event.preventDefault();
+    const delay = +event.currentTarget.elements.delay.value;
+    const btnRadio = event.currentTarget.elements.state.value;
+
+    createPromise(btnRadio, delay)
+        .then(res => {
+            iziToast.success({
+                message: `✅ Fulfilled promise in ${delay}ms`
+            });
+        })
+        .catch(error => {
+            iziToast.error({
+                message: `❌ Rejected promise in ${delay}ms`
+            });
+        });
+
+    event.currentTarget.removeEventListener('submit', onForSubmit);
+}
+
+function createPromise(btn, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (btn === 'fulfilled') {
+                resolve({ delay });
+            } else {
+                reject({ delay });
+            }
+        }, delay);
+    });
+}
